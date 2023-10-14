302ff6417dbc5fb1db78cb698ac792d2eabc406e
teodorobenjamin <143895452+teodorobenjamin@users.noreply.github.com>
2023-10-14 13:54:06 -0400

    Add files via upload
    examen teodoro humerez

---
+++ main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { Login } from './components/Login.jsx';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Login />
+  </React.StrictMode>,
+)
